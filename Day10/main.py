bced49bd4c8f1de33bed86bb3242d028b19a49fb
Karol Tracz <karoltracz@duck.com>
2025-04-20 22:24:37 +0200

    Day10, To many paths. Score over expected value

--- Day10/main.py
+++ Day10/main.py
@@ -1,10 +1,12 @@
+from typing import List, Tuple
+
 with open('input_test.txt', 'r') as f:
 # with open('input.txt', 'r') as f:
     raw_data = f.readlines()
     data = [i.strip() for i in raw_data]
 
 
-def find_zero(height_map: [str]) -> [(int, int), (int, int)]:
+def find_zero(height_map: [str]) -> List[Tuple[int, int]]:
     zeros = []
 
     for i, row in enumerate(height_map):
@@ -14,37 +16,64 @@ def find_zero(height_map: [str]) -> [(int, int), (int, int)]:
     return zeros
 
 
-def find_neighbour(height_map:[str], position: (int, int), seeking_value: int) -> [(True, (int, int))]:
+def find_neighbour(height_map:[str], position: (int, int), seeking_value: int) -> List[Tuple[True, Tuple[int]]]:
     result = []
 
     neighbours = ((-1, 0), (0, -1), (1, 0), (0, 1))
-    try:
-        for i in neighbours:
-            x, y = position[0] + i[0], position[1] + i[1]
-            if x < 0 or y < 0:
-                continue
-            if height_map[x][y] == seeking_value:
-                result.append((True, (x, y)))
-            else:
-                result.append((False, (x, y)))
-    except IndexError:
-        pass
+    for dx, dy in neighbours:
+        x, y = position[0] + dx, position[1] + dy
+        if x < 0 or y < 0:
+            continue
+        try:
+            val = int(height_map[x][y])
+        except IndexError:
+            continue
+        if val == seeking_value:
+            result.append((True, (x, y)))
+        else:
+            result.append((False, (x, y)))
 
     return result
 
 
-def fine_all_1_to_9(height_map, start_position:(int, int)):
-    slope = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    for i in slope:
-        list_ = find_neighbour(height_map=height_map, position=start_position, seeking_value=i)
-        print(list_)
+def find_all_1_to_9(height_map, start_position: Tuple[int]):
+    score = 0
+    slope = (2, 3, 4, 5, 6, 7, 8, 9)
+    paths = []
+
+    neighbours = find_neighbour(height_map=height_map, position=start_position, seeking_value=1)
+    for neighbour in neighbours:
+        if neighbour[0]:
+            paths.append((1, neighbour[1]))
+        else:
+            pass
+
+    for path in paths:
+        for i in slope:
+            if (path[0] + 1) == i:
+                start_position = path[1]
+            else:
+                continue
+            neighbours = find_neighbour(height_map=height_map, position=start_position, seeking_value=i)
+            for neighbour in neighbours:
+                if neighbour[0] == True:
+                    paths.append((i, neighbour[1]))
+                else:
+                    pass
+
+    for path in paths:
+        if path[0] == 9:
+            score += 1
+    return score
 
 
 def first_star(data):
+    result = 0
     zeros = find_zero(height_map=data)
     for position in zeros:
-        fine_all_1_to_9(height_map=data, start_position=position)
-    print(zeros)
+        result += find_all_1_to_9(height_map=data, start_position=position)
+    print(f'first_star {result=}')
+
 
 def second_star():
     pass
