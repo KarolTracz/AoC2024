60295eaf1424a17899ee08c45e8f50b718e44c6c
Karol Tracz <karoltracz@duck.com>
2025-04-20 13:53:07 +0200

    day 10 initial commit

---
+++ Day10/main.py
@@ -0,0 +1,17 @@
+# with open('input_test.txt', 'r') as f:
+with open('input.txt', 'r') as f:
+    raw_data = f.readlines()
+    data = [i.strip() for i in raw_data]
+
+
+def first_star():
+    pass
+
+
+def second_star():
+    pass
+
+
+if __name__ == '__main__':
+    first_star()
+    second_star()
