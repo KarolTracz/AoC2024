fddc5b91ea343b6c80b9f36ca610c22df8e1c7af
KarolTracz <KTracz00@gmail.com>
2025-04-26 19:44:31 +0200

    Day11 initial commit

---
+++ Day11/main.py
@@ -0,0 +1,21 @@
+from typing import List, Tuple
+
+# with open('input_test.txt', 'r') as f:
+with open('input.txt', 'r') as f:
+    raw_data = f.readlines()
+    data = [i.strip() for i in raw_data]
+
+
+def first_star(data):
+    result = 0
+    print(f'first_star {result=}')
+
+
+def second_star(data):
+    result = 0
+    print(f'first_star {result=}')
+
+
+if __name__ == '__main__':
+    first_star(data)
+    second_star(data)
