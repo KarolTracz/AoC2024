3e2cf2d0f63e7aefcb8e12880e06cccaaf03d893
KarolTracz <KTracz00@gmail.com>
2025-05-06 10:35:17 +0200

    Day13, first start resolve

--- Day13/main.py
+++ Day13/main.py
@@ -11,17 +11,29 @@ def first_star(data):
 
     arcade_machines = []
 
-    for i, line in enumerate(data):
-        if i % 3 == 0:
-            A_button = [int(i) for i in line]
-            B_button = [int(i) for i in data[i + 1]]
-            Prize = [int(i) for i in data[i + 2]]
-            arcade_machines.append({'A': A_button, 'B': B_button, 'Prize': Prize})
+    for machine, line in enumerate(data):
+        if machine % 3 == 0:
+            a_button = [int(i) for i in line]
+            b_button = [int(i) for i in data[machine + 1]]
+            prize = [int(i) for i in data[machine + 2]]
+            arcade_machines.append({'A': a_button, 'B': b_button, 'Prize': prize})
 
-    for i in arcade_machines:
-        print(i)
+    for machine in arcade_machines:
+        tokens = 0
 
+        a_x, a_y = machine['A']
+        b_x, b_y = machine['B']
+        goal_x, goal_y = machine['Prize']
 
+        for a_press in range(101):
+            for b_press in range(101):
+                if a_x * a_press + b_x * b_press == goal_x and a_y * a_press + b_y * b_press == goal_y:
+                    print(f'{a_press=} {b_press=} {machine=}')
+                    tokens = 3 * a_press + b_press
+                    break
+                    # print(f'{A_x} * {A_press} + {B_x} * {B_press} == {goal_x}')
+                    # print(f'{A_y} * {A_press} + {B_y} * {B_press} == {goal_y}')
+        result += tokens
 
     print(f'first_star {result=}')
 
